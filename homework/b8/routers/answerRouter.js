b1d4f07c25025f3f8a89341ee427cf95fa0980f2
cuonghx <cuonghx2709@gmail.com>
2018-03-28 00:13:59 +0700

    update ajax for answer

--- homework/b8/routers/answerRouter.js
+++ homework/b8/routers/answerRouter.js
@@ -7,15 +7,17 @@ Router.get('/yes/:id', (req, res) => {
     questionController.updateYesNo(true, id, (err) =>{
         if(err) console.log(err);
         console.log("success!");
-        res.redirect('/question/' + id);
+        res.send(id);
+        // res.redirect('/question/' + id);
     });
 });
 Router.get('/no/:id', (req, res) => {
     let id = req.params.id;
+    console.log(id);
     questionController.updateYesNo(false, id, (err) =>{
         if(err) console.log(err);
         console.log("success!");
-        res.redirect('/question/' + id);
+        res.send(id);
     });
 });
  module.exports = Router;
